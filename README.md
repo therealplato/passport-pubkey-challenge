4d8b021808a94c3cb984bc0450d169fd450d0e26
Plato <therealplato@gmail.com>
2013-05-19 18:59:11 -0400

    Add passport-google skeleton and some outlines in readme

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 passport-pubkey-challenge
 =========================
 
+**The strategy is not implemented yet**
+
 Passport strategy for pubkey auth - successful decryption of an encrypted challenge demonstrates ownership of a public key
+
+
+GET /auth/gpg/start/5FC25A1E41602E3F99D8090381898844A1BF37D6
+Create and securely store
+  challenge = {
+    timestamp: new Date().valueOf(),
+    secret: crypto.random(),
+    fingerprint: '5FC25A1E41602E3F99D8090381898844A1BF37D6', 
+  }
+
+Or use a mem cache:
+  challenges = {
+    _5FC25A1E41602E3F99D8090381898844A1BF37D6: {
+      timestamp: new Date().valueOf(),
+      secret: crypto.random(),
+    }
+  };
+
+Send response to auth request:
+  <ciphertext encrypted to the key matching this fingerprint>
+    the plaintext:
+    https://server.tld/auth/gpg/callback?challenge=some-crypto-random-number&fingerprint=asdf
+
+GET /auth/gpg/callback?challenge=correct-number&fingerprint=asdf
+fetch user associated with this fingerprint; passport.callback(user);
+
+GET /auth/gpg/callback?challenge=wrong-number
+404
---
+++ examples/signon/app.js
@@ -0,0 +1,122 @@
+var express = require('express')
+  , passport = require('passport')
+  , util = require('util')
+  , GPGStrategy = require('../../lib/passport-gnupg').Strategy;
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, we use dummy users.
+
+passport.serializeUser(function(user, done) {
+  done(null, user.fp);
+});
+
+passport.deserializeUser(function(fingerprint, done) {
+  var user = {
+    fp: fingerprint,
+    pubkey: null,
+  };
+
+  done(null, user);
+});
+
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in passport require a `validate` function, which accept
+//   credentials (in this case, an OpenID identifier and profile), and invoke a
+//   callback with a user object.
+passport.use(new GPGStrategy({
+    returnURL: 'http://localhost:3000/auth/gpg/callback',
+  },
+  function(fingerprint, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      var user = {
+        fp: fingerprint,
+        pubkey: null,
+      };
+      return done(null, user);
+      
+      // To keep the example simple, we return a dummy user. In a typical
+      // application, you would want to associate the fingerprint with a user
+      // record in your database, and return that user instead.
+    });
+  }
+));
+
+
+var app = express.createServer();
+
+// configure Express
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.logger());
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'cop bites donut' }));
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/../../public'));
+});
+
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+// GET /auth/google
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Google authentication will involve redirecting
+//   the user to google.com.  After authenticating, Google will redirect the
+//   user back to this application at /auth/google/return
+app.get('/auth/google', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+// GET /auth/google/return
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/google/return', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.listen(3000);
+
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
