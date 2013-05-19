0fa9e49cc932db478b9ad8690cecfb996d4f2e87
Plato <therealplato@gmail.com>
2013-05-19 19:53:40 -0400

    Fix strategy require

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -1,7 +1,7 @@
 var vows = require('vows');
 var assert = require('assert');
 var util = require('util');
-var GoogleStrategy = require('passport-google/strategy');
+var GoogleStrategy = require('passport-google').Strategy;
 
 
 vows.describe('GoogleStrategy').addBatch({
